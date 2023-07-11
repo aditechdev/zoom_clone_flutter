1c19a35ac49887db08330976402ee737479ecb69
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-11 23:37:16 +0530

    :rocket: Home Screen UI done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,101 @@
+import 'package:color_log/color_log.dart';
 import 'package:flutter/material.dart';
+import 'package:zoom_clone_flutter/utils/colors.dart';
+import 'package:zoom_clone_flutter/widget/home_meeting_button.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentPageIndex = 0;
+  onPageChanged(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("APPP BAR"),
+        elevation: 0,
+        title: const Text("Meet & Chat"),
+        centerTitle: true,
+        backgroundColor: backgroundColor,
       ),
-      body: const Text("data"),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              HomeMeatingButtonWidget(
+                onTap: () {
+                  clog.checkSuccess(true, "It is working");
+                },
+                text: "New Meeting",
+                icon: Icons.videocam,
+              ),
+              HomeMeatingButtonWidget(
+                onTap: () {
+                  clog.checkSuccess(true, "It is working");
+                },
+                text: "Join Meeting",
+                icon: Icons.add_box_rounded,
+              ),
+              HomeMeatingButtonWidget(
+                onTap: () {
+                  clog.checkSuccess(true, "It is working");
+                },
+                text: "Schedule",
+                icon: Icons.calendar_month,
+              ),
+              HomeMeatingButtonWidget(
+                onTap: () {
+                  clog.checkSuccess(true, "It is working");
+                },
+                text: "Share Screen",
+                icon: Icons.arrow_upward,
+              ),
+            ],
+          ),
+          const Expanded(
+            child: Center(
+              child: Text(
+                "Create/Join meetings with just one click!",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: footerColor,
+          currentIndex: _currentPageIndex,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.white,
+          onTap: onPageChanged,
+          unselectedFontSize: 14,
+          selectedFontSize: 14,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.comment_bank), label: "Meet & Chat"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.lock_clock), label: "Meetings"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined), label: "Contacts"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined), label: "Settings"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.comment_bank), label: "Meet & Chat"),
+          ]),
     );
   }
 }
---
+++ lib/widget/home_meeting_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone_flutter/utils/colors.dart';
+
+class HomeMeatingButtonWidget extends StatelessWidget {
+  final VoidCallback onTap;
+  final IconData? icon;
+  final String? text;
+
+  const HomeMeatingButtonWidget({super.key, required this.onTap, this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              color: buttonColor,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 10,
+                    color: Colors.black.withOpacity(0.06),
+                    offset: const Offset(0, 4))
+              ],
+            ),
+            child: Icon(
+              icon,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            text!,
+            style: TextStyle(color: Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+}

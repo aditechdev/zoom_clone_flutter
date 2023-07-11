7d1ba23d650c2c0fe62de68687a535a797df843d
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-11 22:06:15 +0530

    :rocket: SetUp: Firebase setup successfully

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,19 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:zoom_clone_flutter/r.dart';
+import 'package:zoom_clone_flutter/resources/auth_methods.dart';
 import 'package:zoom_clone_flutter/widget/custom_button.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final AuthMethods _authMethods = AuthMethods();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +35,14 @@ class LoginScreen extends StatelessWidget {
         ),
         CustomButtonWidget(
           btnText: "Google Sign In",
-          onTap: () {},
+          onTap: () async {
+            bool res = await _authMethods.signInWithGoogle(context);
+            log("==============> $res");
+
+            if (res) {
+              Navigator.pushNamed(context, "/home");
+            }
+          },
         )
       ],
     ));

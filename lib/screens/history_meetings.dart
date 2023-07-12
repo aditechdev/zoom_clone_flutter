1daed3a7f49ddb63ce5adc2449d676d000b60d29
Aditya Anand <aditechdevofficial@gmail.com>
2023-07-12 12:28:49 +0530

    :stethoscope: Fixed jitsi for android

---
+++ lib/screens/history_meetings.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HistoryMeetingsScreen extends StatelessWidget {
+  const HistoryMeetingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
